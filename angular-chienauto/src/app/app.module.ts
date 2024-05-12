9a97ecfc28af3eb2ba9ee2f60a02786334a3e69f
Hnamm <hainam190403@gmail.com>
2024-05-12 14:27:36 +0700

    add danhmuc

--- angular-chienauto/src/app/app.module.ts
+++ angular-chienauto/src/app/app.module.ts
@@ -8,7 +8,19 @@ import { ProductService } from './services/product.service';
 import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
 
+
+
+const routes: Routes = [
+    {path: 'productCategory/:id', component: ProductListComponent},
+    {path: 'productCategory/', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    {path: '**', redirectTo: '/products', pathMatch: 'full'},
+
+]
 
 
 @NgModule({
@@ -21,6 +33,7 @@ import { FooterComponent } from './component/footer/footer.component';
 
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
 
